d6af2b5caf7f073ba46ff30045396761e00f0a8f
UserPL135Pc06 <UserPL135Pc06@SLFRPL135PC06.ensino.atec.local>
2016-10-26 11:33:31 +0100

    Calculadora Balanço

--- src/Principal.java
+++ src/Principal.java
@@ -7,7 +7,16 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Olá Aleijados");
+		
+		
+		int quotas = 300, recbaile = 200, recbar = 250;
+		int totalReceitas = quotas + recbaile + recbar;
+		System.out.println("Receita: "+totalReceitas);
+		int agua = 100, luz = 60, telefone = 70, totalDespesas;
+		totalDespesas = agua + luz + telefone;
+		System.out.println("Despesa: "+totalDespesas);
+		int Lucro = totalReceitas - totalDespesas;
+		System.out.println("Lucro: "+Lucro);
 	}
 
 }
